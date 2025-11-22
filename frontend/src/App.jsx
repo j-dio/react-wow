8698247e1c1e0b00d07c699153ab1765676389e0
John Dio Lumacang <tcshs.lumacang.johndio.a@gmail.com>
2025-11-22 14:30:21 +0800

    clean workspace

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Text />
+      <Text />
+    </>
+  );
+}
+
+function Text() {
+  return (
+    <div>
+      <p>hello world</p>
+    </div>
+  );
+}
+export default App;
