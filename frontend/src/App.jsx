9af31207ed3b1f0e1eb2d23877cc968d7e5b5c9a
John Dio Lumacang <tcshs.lumacang.johndio.a@gmail.com>
2025-11-23 13:47:00 +0800

    almost done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,21 @@
-import "./App.css";
+import "./css/App.css";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import Favorites from "./pages/Favorites.jsx";
+import NavBar from "./components/NavBar.jsx";
 
 function App() {
   return (
-    <>
-      <Text />
-      <Text />
-    </>
-  );
-}
-
-function Text() {
-  return (
     <div>
-      <p>hello world</p>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
     </div>
   );
 }
+
 export default App;
